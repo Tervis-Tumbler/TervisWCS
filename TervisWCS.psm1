968086d2da0f5b529910b3275a7976342c4ac49d
Chris Magnuson <hourglasssand@gmail.com>
2017-03-22 17:10:11 -0400

    Initial code to install all local WCS printers needed for BartenderCommander

--- TervisWCS.psm1
+++ TervisWCS.psm1
@@ -42,3 +42,92 @@ DatabaseName=$DatabaseName
     New-ItemProperty -Path HKLM:\SOFTWARE\ODBC\ODBC.INI\$DatabaseName -PropertyType String -Name UID -Value $Credential.UserName
     New-ItemProperty -Path HKLM:\SOFTWARE\ODBC\ODBC.INI\$DatabaseName -PropertyType String -Name PWD -Value $Credential.GetNetworkCredential().password
 }
+
+function Get-WCSEquipment {
+    $SybaseDatabaseEntryDetails = Get-PasswordstateSybaseDatabaseEntryDetails -PasswordID 3459
+    $ConnectionString = $SybaseDatabaseEntryDetails | ConvertTo-SQLAnywhereConnectionString
+
+    $Query = @"
+SELECT * FROM "qc"."Equipment"
+"@
+    Invoke-SQLAnywhereSQL -ConnectionString $ConnectionString -SQLCommand $Query -DatabaseEngineClassMapName SQLAnywhere -ConvertFromDataRow
+}
+
+function Get-WCSEquipmentBottomLabelPrintEngine {
+    Get-WCSEquipment |
+    where id -Match _PL
+}
+
+function Install-ZDesignerDriverForWindows10AndLaterFromWindowsUpdate {
+    param (
+        $ComputerName
+    )
+    Invoke-Command -ScriptBlock {
+        $ZebraDriver = get-windowsdriver -Online | 
+        where providername -eq zebra |
+        where ClassName -eq Printer |
+        where Version -eq 5.1.7.6290
+
+        if (-not $ZebraDriver) {
+            Invoke-WebRequest http://download.windowsupdate.com/c/msdownload/update/driver/drvs/2016/06/20857735_abfb8f058ce8dd7bbb70ec4a7df3947f81b204a8.cab -OutFile $env:TEMP\ZDesigner.cab
+            New-Item -Path $env:TEMP\zdesigner -ItemType Directory
+            expand.exe -F:* $env:TEMP\zdesigner.cab $env:TEMP\zdesigner
+            pnputil.exe -i -a $env:TEMP\zdesigner\ZBRN.inf
+        }
+    } @PSBoundParameters
+}
+
+function Install-WCSPrintersForBartenderCommander {
+    param (
+        $ComputerName
+    )
+    Install-ZDesignerDriverForWindows10AndLaterFromWindowsUpdate @PSBoundParameters
+    Add-PrinterDriver -Name "ZDesigner 110Xi4 203 dpi" @PSBoundParameters
+    
+    Get-WCSEquipmentBottomLabelPrintEngine |
+    Add-LocalWCSPrinter @PSBoundParameters
+}
+
+function Add-LocalWCSPrinter {
+    param(
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]$ID,
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]$HostID,
+        $ComputerName
+    )
+    begin {
+        $ComputerNameParameter = $PSBoundParameters | 
+        ConvertFrom-PSBoundParameters | 
+        where ComputerName
+    }
+    process {
+        if ($ComputerName) {
+            Add-PrinterPort -Name $HostID -PrinterHostAddress $HostID -ComputerName $ComputerName
+            Add-Printer -PortName $HostID -Name $ID -DriverName "ZDesigner 110Xi4 203 dpi" -ComputerName $ComputerName 
+        } else {
+            Add-PrinterPort -Name $HostID -PrinterHostAddress $HostID 
+            Add-Printer -PortName $HostID -Name $ID -DriverName "ZDesigner 110Xi4 203 dpi"
+        }
+    }
+}
+
+function Remove-LocalWCSPrinter {
+    param(
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]$ID,
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]$HostID,
+        $ComputerName
+    )
+    begin {
+        $ComputerNameParameter = $PSBoundParameters | 
+        ConvertFrom-PSBoundParameters | 
+        where ComputerName
+    }
+    process {
+        if ($ComputerName) {
+            Remove-Printer -Name $ID -ComputerName $ComputerName 
+            Remove-PrinterPort -Name $HostID -ComputerName $ComputerNameet
+        } else {
+            Remove-Printer -Name $ID
+            Remove-PrinterPort -Name $HostID
+        }
+    }
+}
